ab0cf61d3a2fab9dda23f40969e0e9d5b4e2befd
Холодов Алексей <holodovrpk@gmail.com>
2025-12-02 10:29:51 +0300

    eee

---
+++ EF227_Demo/Models/Priem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF227_Demo.Models
+{
+    public class Priem
+    {
+        public int PriemId { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeSpan Time_pr { get; set; }
+        public string Result { get; set; }
+
+        public int DoctorId { get; set; }
+        
+        [ForeignKey("DoctorId")]
+        public Doctor Doctor { get; set; }
+
+        public int PatientId { get; set; }
+        [ForeignKey("PatientId")]
+
+        public Patient Patient { get; set; }
+    }
+}
